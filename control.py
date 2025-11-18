d71a2dd4038b9e5079dd66f41835e4e9a52410b2
Jónas Holm Wentzlau <jonas@wentzlau.dk>
2025-11-18 13:40:49 +0100

    Inverse kinematics works i guess

--- control.py
+++ control.py
@@ -1,6 +1,7 @@
 import dynamixel_sdk as dxl
 import kinematics
 import numpy as np
+from time import sleep
 
 ADDR_MX_TORQUE_ENABLE = 24
 ADDR_MX_CW_COMPLIANCE_MARGIN = 26
@@ -32,7 +33,7 @@ def setup_motors():
         packetHandler.write2ByteTxRx(portHandler, DXL_ID, ADDR_MX_MOVING_SPEED, 100)
     return portHandler, packetHandler
 
-def deg2dxl(servo,deg,opts=[150,150,45,245]):
+def deg2dxl(servo,deg,opts=[150,58,133,245]):
     match servo:
         case 1:
             #512=0, 240=a*90+512
@@ -51,19 +52,38 @@ def set_angles(q):
         if deg2dxl(id+1,np.rad2deg(q[id]))>=0 and deg2dxl(id+1,np.rad2deg(q[id]))<=1023:
             packetHandler.write2ByteTxRx(portHandler, id+1, ADDR_MX_GOAL_POSITION, deg2dxl(id+1,np.rad2deg(q[id])))
         else:
-            print("Angle out of range for servo ", id+1)
+            print("Angle out of range for servo ", id+1,": ", deg2dxl(id+1,np.rad2deg(q[id])))
     
 
-portHandler, packetHandler = setup_motors()
-T_03,T_04, T_05 = kinematics.forwards_kinematics(np.deg2rad(10), np.deg2rad(10), np.deg2rad(0), np.deg2rad(0))
-q = kinematics.inverseKinematics(T_04[0:3,0],T_04[0:3,3])
-#q = kinematics.inverseKinematics([1,0,0], [150,0,50])
-print(T_04[0:3,3])
-set_angles([q[0],q[1][0],q[2][0],q[3][0]])
-#print("q = ", np.rad2deg(q[0]), np.rad2deg(q[1][0]), np.rad2deg(q[2][0]), np.rad2deg(q[3][0]))
-#packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, deg2dxl(1,np.rad2deg(q[0])))
-#packetHandler.write2ByteTxRx(portHandler, 2, ADDR_MX_GOAL_POSITION, deg2dxl(2,np.rad2deg(q[1][0])))
-#packetHandler.write2ByteTxRx(portHandler, 3, ADDR_MX_GOAL_POSITION, deg2dxl(3,np.rad2deg(q[2][0])))
-#packetHandler.write2ByteTxRx(portHandler, 4, ADDR_MX_GOAL_POSITION, deg2dxl(4,np.rad2deg(q[3][0])))
+def calculate_circle_step(phi):
+    p_c = np.array([150, 0, 120])  # Center of the circle
+    radius = 32  # Radius of the circle
+    rot = np.array([0,np.cos(phi),np.sin(phi)])
+    return p_c + radius * rot
 
-print(deg2dxl(4,np.rad2deg(q[3][0])))
+
+if __name__ == "__main__":
+    # Test the functions
+    portHandler, packetHandler = setup_motors()
+    #T_03,T_04, T_05 = kinematics.forwards_kinematics(np.deg2rad(0), np.deg2rad(0), np.deg2rad(0), np.deg2rad(0))
+    #q = kinematics.inverseKinematics(T_04[0:3,0],T_04[0:3,3])
+    #q = kinematics.inverseKinematics([1,0,0], [143,0,143])
+    #print(np.rad2deg([q[0],q[1][0],q[2][0],q[3][0]]))
+    #set_angles([q[0],q[1][1],q[2][1],q[3][1]])
+
+    for i in range(36):
+        q = kinematics.inverseKinematics([0,0,0],calculate_circle_step(2*np.pi/36*i))
+        print(np.rad2deg(q[0]), np.rad2deg(q[1][0]), np.rad2deg(q[2][0]), np.rad2deg(q[3][0]))
+        print(deg2dxl(1,np.rad2deg(q[0])), deg2dxl(2,np.rad2deg(q[1][0])), deg2dxl(3,np.rad2deg(q[2][0])), deg2dxl(4,np.rad2deg(q[3][0])))
+        #set_angles([q[0]), deg2dxl(2,np.rad2deg(q[1][0])), deg2dxl(3,np.rad2deg(q[2][0])), deg2dxl(4,np.rad2deg(q[3][0]))])
+        set_angles([q[0],q[1][1],q[2][1],q[3][1]])
+        sleep(0.2)
+    #sleep(2)
+    #set_angles([q[0],q[1][1],q[2][1],q[3][1]])
+    #print("q = ", np.rad2deg(q[0]), np.rad2deg(q[1][0]), np.rad2deg(q[2][0]), np.rad2deg(q[3][0]))
+    #packetHandler.write2ByteTxRx(portHandler, 1, ADDR_MX_GOAL_POSITION, deg2dxl(1,np.rad2deg(q[0])))
+    #packetHandler.write2ByteTxRx(portHandler, 2, ADDR_MX_GOAL_POSITION, deg2dxl(2,np.rad2deg(q[1][0])))
+    #packetHandler.write2ByteTxRx(portHandler, 3, ADDR_MX_GOAL_POSITION, deg2dxl(3,np.rad2deg(q[2][0])))
+    #packetHandler.write2ByteTxRx(portHandler, 4, ADDR_MX_GOAL_POSITION, deg2dxl(4,np.rad2deg(q[3][0])))
+
+    #print(deg2dxl(4,np.rad2deg(q[3][0])))
